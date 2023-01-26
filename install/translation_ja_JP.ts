180c6dd4c2de96a33f5ceea6d5418e95da3b35b2
starfieldsKt <keita.hoshino.1001@gmail.com>
2023-01-26 17:53:47 +0900

    翻訳ファイルを追加

---
+++ install/translation_ja_JP.ts
@@ -0,0 +1,48 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TS version="2.0" language="ja_JP">
+    <!--これは iRIC で使用する辞書ファイルです。
+<translation> と </translation> の間に、翻訳後の文字列を追記してください。
+-->
+    <context>
+        <message>
+            <source>Grid Tester</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>readme</source>
+            <translation>説明</translation>
+        </message>
+        <message>
+            <source>・No need to set anything.</source>
+            <translation>・特に何も設定する必要はありません</translation>
+        </message>
+        <message>
+            <source>・Just run the calculation and the grid elevation will be output.</source>
+            <translation>・計算を実行れば計算結果に格子と標高が出力されます</translation>
+        </message>
+        <message>
+            <source>・Bonus features are optional.</source>
+            <translation>・おまけ機能で格子全体に一定の水位を与えることもできます</translation>
+        </message>
+        <message>
+            <source>omake mode</source>
+            <translation>おまけ機能</translation>
+        </message>
+        <message>
+            <source>time step of output(sec)</source>
+            <translation>出力間隔時間(秒)</translation>
+        </message>
+        <message>
+            <source>Water level(m)</source>
+            <translation>水位(m)</translation>
+        </message>
+        <message>
+            <source>Time(sec)</source>
+            <translation>時間(秒)</translation>
+        </message>
+        <message>
+            <source>Elevation(m)</source>
+            <translation>地形高(m)</translation>
+        </message>
+    </context>
+</TS>
